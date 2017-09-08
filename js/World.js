8a72db09d0594d45f15cff6eea8a07387bcf2e29
Terrence <t.w.mcdonnell10@gmail.com>
2017-09-08 14:35:08 -0700

    fixing out of bounds glitch

--- js/World.js
+++ js/World.js
@@ -2,7 +2,7 @@ const WORLD_W = 20;
 const WORLD_H = 20;
 const WORLD_GAP = 0;
 const WORLD_COLS = 16;
-const WORLD_ROWS = 9;
+const WORLD_ROWS = 10;
 //rooms are defined now in rooms.js
 var worldGrid = [];
 
