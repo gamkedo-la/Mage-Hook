1499d7f2e4a50326117049b0f22177cd10a895e9
Terrence <t.w.mcdonnell10@gmail.com>
2017-09-13 12:04:52 -0700

    tile editor code "find tile array # under mouse" /ht siggyrath

--- js/TileEditor.js
+++ js/TileEditor.js
@@ -2,8 +2,8 @@ var _DEBUG_ENABLE_TILE_EDITOR = false;
 
 //console.log(currentRoom.layout);
 function roomTileCoordinate() {
-    roomCol = Math.floor(mouseX / WORLD_W);
-    roomRow = Math.floor(mouseY / WORLD_H);
+    roomCol = Math.floor(WORLD_COLS * mouseX / canvas.offsetWidth);
+    roomRow = Math.floor(WORLD_ROWS * mouseY / canvas.offsetHeight);
 
    // var tileX = (roomCol * WORLD_W);
    // var tileY = (roomRow * WORLD_H);
