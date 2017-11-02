b213157f26a37ffe68fff83a7df583b99e4d3e10
Christer Kaitila <github@mcfunkypants.com>
2017-11-01 18:58:24 -0700

    screenshake margin bug fixed

--- js/Screenshake.js
+++ js/Screenshake.js
@@ -4,16 +4,22 @@
 var screen_shakes = 0; // frames of screenshake used as player feedback for when we take damage
 var screen_shake_pivot_x = 0;
 var screen_shake_pivot_y = 0;
-var screen_shake_me = document.getElementById('gameCanvas');
+var screen_shake_me = document.body;//getElementById('gameCanvas');
+var original_css = null;
+
 var MAX_SCREEN_SHAKES = 30;
+var MAX_SCREEN_SHAKE_SIZE_PX = 20;
+var CSS_PROPERTY_TO_SHAKE = "margin"; // could be padding, margin, left etc
+
 function screenShake(howmany)
 {
 	//console.log('screenshake ' + howmany);
 	if (!screen_shake_me)
 	{
-		console.log('ERROR: screenshake does not know which element to shake!')
+		console.log('ERROR: screenshake does not know which html element to shake!')
 		return;
 	}
+
 	screen_shake_pivot_x = 0;
 	screen_shake_pivot_y = 0;
 	screen_shakes += howmany;
@@ -29,11 +35,15 @@ function updateScreenshake()
 
 	if (screen_shakes>0)
 	{
+		if (original_css==null) // remember initial setting
+			original_css = screen_shake_me.style[CSS_PROPERTY_TO_SHAKE];
+
 		var shakesize = screen_shakes / 2;
-		if (shakesize > 20) shakesize = 20;
+		if (shakesize > MAX_SCREEN_SHAKE_SIZE_PX) 
+			shakesize = MAX_SCREEN_SHAKE_SIZE_PX;
 
 		// shake around a pivot point
-		screen_shake_me.style.margin = "" + (screen_shake_pivot_x + Math.round((Math.random() * shakesize) - shakesize / 2) * 2) + 
+		screen_shake_me.style[CSS_PROPERTY_TO_SHAKE] = "" + (screen_shake_pivot_x + Math.round((Math.random() * shakesize) - shakesize / 2) * 2) + 
 					"px " + (screen_shake_pivot_y + Math.round((Math.random() * shakesize) - shakesize / 2) * 2) + "px";
 
 		screen_shakes--;
@@ -43,7 +53,7 @@ function updateScreenshake()
 		if (screen_shakes < 1)
 		{
 			//console.log('screenshakes done. going back to original position.')
-			screen_shake_me.style.margin = "0";
+			screen_shake_me.style[CSS_PROPERTY_TO_SHAKE] = original_css; //"0";
 		}
 	}
 }
