49ceb3494d1457b2b8b55b06be86b02eab763877
Christer Kaitila <github@mcfunkypants.com>
2017-11-04 17:15:58 -0700

    anchor spell particle effect

--- js/magicSpells/anchorSpell.js
+++ js/magicSpells/anchorSpell.js
@@ -17,24 +17,39 @@ function anchorMagic(x, y, isFacing) {
 	this.spriteSpeed = 18;
 	
 	this.attackDir = [0,0];
+
+	var particleVX = 0;
+	var particleVY = 0;
 	
 	switch(this.isFacing) { //Draw attack in facing dirction
 			case NORTH:
 				this.x -= 6;
 				this.y -= 16;
+				particleVX = 0.001;
+				particleVY = -2;
 				break;
 			case SOUTH:
 				this.x -= 6;
 				this.y += 16;
+				particleVX = -0.001;
+				particleVY = 2;
 				break;
 			case EAST:
 				this.x += 13;
+				particleVX = 2;
+				particleVY = 0.001;
 				break;
 			case WEST:
 				this.x -= 25;
+				particleVX = -2;
+				particleVY = -0.001;
 				break;
 		}
 	
+	// blue sparks placed with offset for facing dir so it starts close to player
+	particleFX(x,y,24,'rgba(0,240,255,0.33)',particleVX,particleVY,0.4,0.0,1.0);
+	
+
 	this.onHitEnemy = function (enemy) {
 		console.log('WE HIT AN ENEMY!!!!');
 		player.enemyHitCount++;
